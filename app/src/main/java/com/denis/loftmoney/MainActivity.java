87d14ed19627803c4f55e5eac5676de09aa6cf1d
Denis <vladimir-avit@yandex.ru>
2020-07-18 20:44:33 +0300

    added a second screen

--- app/src/main/java/com/denis/loftmoney/MainActivity.java
+++ app/src/main/java/com/denis/loftmoney/MainActivity.java
@@ -2,7 +2,10 @@ package com.denis.loftmoney;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnClick).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newActivity = new Intent(getApplicationContext(), AddItemActivity.class);
+                startActivity(newActivity);
+            }
+        });
     }
 }
